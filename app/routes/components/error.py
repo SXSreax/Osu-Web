84e85927df4d8146d2d4772f0aa1dc3f550ced4e
SXSreax <SX.Sreax0267@Outlook.com>
2026-07-29 13:46:18 +1200

    Implement 404 error page
    Implement 404 error page by creating a function in the error.py and create a html for the data to pass from error.py to 404.html if 404 not_found.

--- app/routes/components/error.py
+++ app/routes/components/error.py
@@ -7,4 +7,11 @@ def unauthorized(e):
     code = "401"
     error_message = "Unauthorized login, you will need an account"
     soln = "Login or signup in to continue"
-    return render_template("error/error.html", error_message=error_message, code=code, soln=soln), 401
+    return render_template("error/401.html", error_message=error_message, code=code, soln=soln), 401
+
+@error_bp.app_errorhandler(404)
+def not_found(e):
+    code = "404"
+    error_message = "Page not found"
+    soln = "Redirct to home page"
+    return render_template("error/404.html", error_message=error_message, code=code, soln=soln), 404
--- app/routes/pages/user.py
+++ app/routes/pages/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, flash, redirect, url_for, current_app, request, jsonify, session
+from flask import Blueprint, render_template, flash, redirect, url_for, current_app, request, jsonify, session, abort
 from flask_login import current_user, login_required
 from flask_mail import Message
 from app.extensions import mail
